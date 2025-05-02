0a01a1c883349a68229fb9e32effa4fd475cabf4
yhawbee1 <antwibrian82@gmail.com>
2025-05-02 17:15:12 +0000

    refactor: Update error handling in authenticate function and remove unused seed and query routes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -113,7 +113,7 @@ export async function authenticate(
         await signIn('credentials', formData);
     } catch (error) {
         if (error instanceof AuthError) {
-            switch (error.type) {
+            switch (error.cause) {
                 case 'CredentialsSignin':
                     return 'Invalid credentials.';
                 default:

5fdf394067bac385309ea0accaaf69baeb8ac909
yhawbee1 <antwibrian82@gmail.com>
2025-05-02 17:12:48 +0000

    Implement authentication using NextAuth with PostgreSQL and credentials provider
    - Added auth configuration in auth.config.ts to handle sign-in pages and authorization callbacks.
    - Created auth.ts to set up NextAuth with a credentials provider, including user validation and password comparison using bcrypt.
    - Implemented middleware in middleware.ts to protect routes and manage authentication flow.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,11 +2,6 @@ import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/font";
-import {
-  fetchRevenue,
-  fetchLatestInvoices,
-  fetchCardData,
-} from "../../lib/data";
 import { Suspense } from "react";
 import {
   CardSkeleton,
@@ -14,29 +9,19 @@ import {
   RevenueChartSkeleton,
 } from "@/app/ui/skeletons";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 export default async function Page() {
-  // const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
-  // const {
-  //   numberOfCustomers,
-  //   numberOfInvoices,
-  //   totalPaidInvoices,
-  //   totalPendingInvoices,
-  // } = await fetchCardData();
+  
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
         <Suspense fallback={<CardSkeleton />}>
           <CardWrapper />
           </Suspense>
